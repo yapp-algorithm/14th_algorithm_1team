a9cb21c4445457503bb43eb5bf6b051c1b9cc5a8
koda <kdkyung57@gmail.com>
2019-02-16 07:03:26 +0900

    feat: Programmers Sorting Level2 by Comparator

---
+++ dakyung/yapp14th-study/src/w1/solution/Solution2_4.java
@@ -0,0 +1,33 @@
+package w1.solution;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Solution2_4 {
+//    private static final int[] NUMBERS = new int[]{3, 30, 34, 5, 9};
+//	private static final int[] NUMBERS = new int[]{12,121}; // 12121
+//    private static final int[] NUMBERS = new int[]{21, 212}; // 21221
+    private static final int[] NUMBERS = new int[]{0,0,0,0};
+
+    public static void main(String[] args) {
+        System.out.println(solution(NUMBERS));
+    }
+
+    public static String solution(int[] numbers) {
+        List<String> list = new ArrayList();
+        for(int i : numbers) list.add(Integer.toString(i));
+//        if(list.get(0) == "0") return "0";
+
+        Collections.sort(list, (a, b) -> {
+            return -Integer.compare(Integer.parseInt(a + b), Integer.parseInt(b + a));
+        });
+
+        if(list.get(0).charAt(0) == '0') return "0";
+
+        StringBuilder sb = new StringBuilder();
+        for(String s : list)
+            sb.append(s);
+        return sb.toString();
+    }
+}
